b97de1a0736d646c687c4834b0f2e1cfa79027b3
hanseungyuun <sfgr1112@naver.com>
2026-04-17 21:48:08 +0900

    feat: 직업 프로필 사진 업로드

--- src/web/main.py
+++ src/web/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from src.database.queries import get_all_jobs_for_web
 import os
 
@@ -14,6 +15,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+os.makedirs("public/images", exist_ok=True)
+app.mount("/images", StaticFiles(directory="public/images"), name="images")
 
 @app.get("/")
 def serve_index():
